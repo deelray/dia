a8cd954eaf5067d1e0029d980b24db7278b40a76
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-01-31 00:35:19 +0200

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const sliderImages = document.querySelectorAll('.slider__image');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let slideIndex = 1;
+
+showImage(slideIndex);
+
+function addImage(n) {
+  showImage(slideIndex += n);
+}
+
+function showImage(n) {
+  if (n > sliderImages.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = sliderImages.length;
+  }
+
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = 'none';
+  }
+
+  sliderImages[slideIndex - 1].style.display = 'block';
+}
+
+nextBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  addImage(1);
+});
+
+prevBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  addImage(-1);
+});
